287e08b602406e6fc5d03ff91f674d4bfb97c4c0
Felix-Danso <117346298+Felix-Danso@users.noreply.github.com>
2023-04-05 09:09:13 +0000

    Add ups

--- usereducer/src/App.js
+++ usereducer/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useReducer} from 'react'
 
-function App() {
+const ACTIONS = {
+    INCREMENT:'increment',
+    DECREMENT:'decrement'
+}
+
+function reducer(state, action){
+  switch (action.type){
+      case ACTIONS.INCREMENT:
+        return{count: state.count + 1}
+      case ACTIONS.DECREMENT:
+       return{count: state.count - 1}
+      default:
+        return state
+  }
+}
+export default function App() {
+const [state, dispatch] = useReducer(reducer,{count:0})
+
+  const increment = () => {
+   dispatch({type: ACTIONS.INCREMENT})
+  };
+
+  const decrement = () => {
+    dispatch({type: ACTIONS.DECREMENT});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='flex justify-center items-center mt-28'>
+      <button className='border w-20 h-16 font-bold' onClick={decrement}>-</button>
+      <span className='flex items-center justify-center font-bold bg-slate-200 w-20 h-16'>{state.count}</span>
+      <button className='border w-20 h-16 font-bold' onClick={increment}>+</button>
+      </div>
   );
 }
 
-export default App;
